578a9e2bec1a713464ca294c3f5022937f92134d
Minister124 <kushaltmg321@gmail.com>
2024-06-15 13:40:21 +0545

    Environment

--- Chapter-1/AboutMyEnvironment/Program.cs
+++ Chapter-1/AboutMyEnvironment/Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace AboutMyEnvironment;
+
+class Program{
+    static void Main(string[] args){
+        Console.WriteLine(Environment.CurrentDirectory);
+        Console.WriteLine(Environment.OSVersion.VersionString);
+        Console.WriteLine("Namespace: {0}", typeof(Program).Namespace);
+    }
+}
