cbfaeefa9e081e6b1ef266ce0edd88f630963174
Minister124 <kushaltmg321@gmail.com>
2024-06-16 10:11:07 +0545

    Operators

--- Chapter-3/Operators/Program.cs
+++ Chapter-3/Operators/Program.cs
@@ -87,4 +87,24 @@ WriteLine($"y     | {y,7} | {y:B8}");
 WriteLine($"x & y | {x & y,7} | {x & y:B8}");
 WriteLine($"x | y | {x | y,7} | {x | y:B8}");
 WriteLine($"x ^ y | {x ^ y,7} | {x ^ y:B8}");
+
+// Left-shift x by three bit columns.
+WriteLine($"x << 3 | {x << 3,7} | {x << 3:B8}");
+// Multiply x by 8.
+WriteLine($"x * 8 | {x * 8,7} | {x * 8:B8}");
+// Right-shift y by one bit column.
+WriteLine($"y >> 1 | {y >> 1,7} | {y >> 1:B8}");
+#endregion
+
+#region Miscellaneous Operators
+int age = 50;
+WriteLine($"The {nameof(age)} variable uses {sizeof(int)} bytes of memory.");
+
+// How many operators in the following statement?
+char firstDigit = age.ToString()[0];
+// There are four operators:
+// = is the assignment operator
+// . is the member access operator
+// () is the invocation operator
+// [] is the indexer access operator
 #endregion
