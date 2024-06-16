0771407a665c3506e010944e0c1ec9832ba7d254
Minister124 <kushaltmg321@gmail.com>
2024-06-16 09:07:05 +0545

    Operators

--- Chapter-3/Operators/Program.cs
+++ Chapter-3/Operators/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿#region Exploring unary Operators
+int a = 4;
+int b = a++;
+int c = 3;
+int d = ++c; //prefix means increment c before assigning it
+WriteLine($"a is {a}, b is {b}");
+WriteLine($"c is {c}, d is {d}");
+#endregion
+
+#region Exploring binary arithmetic operators
+int e = 12;
+int f = 5;
+WriteLine($"e is {e}, f is {f}");
+WriteLine($"e + f = {e + f}");
+WriteLine($"e  f = {e - f}");
+WriteLine($"e * f = {e * f}");
+WriteLine($"e / f = {e / f}");
+WriteLine($"e % f = {e % f}");
+#endregion
