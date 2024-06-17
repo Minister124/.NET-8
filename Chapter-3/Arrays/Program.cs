d156708f2f4826573edc8fcff80aa86a65bd1506
Minister124 <kushaltmg321@gmail.com>
2024-06-17 14:00:44 +0545

    List Pattern Matching with Arrays

--- Chapter-3/Arrays/Program.cs
+++ Chapter-3/Arrays/Program.cs
@@ -65,4 +65,36 @@ for(int row = 0; row <= jagged.GetUpperBound(0); row++){
 #endregion
 
 #region List Pattern Matching with arrays
+int[] sequentialNumbers = {1,2,3,4,5, 6, 7, 8, 9, 10};
+int[] oneTwoNumbers = {1, 2};
+int[] oneTwoTenNumbers = {1, 2, 3, 10};
+int[] oneTwoThreeTenNumbers = { 1, 2, 3, 10 };
+int[] primeNumbers = { 2, 3, 5, 7, 11, 13, 17, 19, 23, 29 };
+int[] fibonacciNumbers = { 0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89 };
+int[] emptyNumbers = { }; // Or use Array.Empty<int>()
+int[] threeNumbers = { 9, 7, 5 };
+int[] sixNumbers = { 9, 7, 5, 4, 2, 10 };
+
+static string CheckArray(int[] values) => values switch
+{
+    [] => "Empty Array",
+    [1, 2, _, 10] => "Contains 1, 2, any single number, 10.",
+    [1, 2, .., 10] => "Contains 1, 2, any range including empty, 10.",
+    [1, 2] => "Contains 1 then 2.",
+    [int item1, int item2, int item3] => $"Has {item1} then {item2} and {item3}",
+    [0, _] => "Starts with 0, the one any other number",
+    [0, ..] => "Starts With 0, then any range of numbers",
+    [2, .. int[] others] => $"Starts with 2, then {others.Length} more numbers",
+    [..] => "Any items in any order",
+};
+
+WriteLine($"{nameof(sequentialNumbers)}: {CheckArray(sequentialNumbers)}");
+WriteLine($"{nameof(oneTwoNumbers)}: {CheckArray(oneTwoNumbers)}");
+WriteLine($"{nameof(oneTwoTenNumbers)}: {CheckArray(oneTwoTenNumbers)}");
+WriteLine($"{nameof(oneTwoThreeTenNumbers)}: {CheckArray(oneTwoThreeTenNumbers)}");
+WriteLine($"{nameof(primeNumbers)}: {CheckArray(primeNumbers)}");
+WriteLine($"{nameof(fibonacciNumbers)}: {CheckArray(fibonacciNumbers)}");
+WriteLine($"{nameof(emptyNumbers)}: {CheckArray(emptyNumbers)}");
+WriteLine($"{nameof(threeNumbers)}: {CheckArray(threeNumbers)}");
+WriteLine($"{nameof(sixNumbers)}: {CheckArray(sixNumbers)}");
 #endregion
