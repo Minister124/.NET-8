d9d76c94983a8751b6b141f5c1694f664dad0054
Minister124 <kushaltmg321@gmail.com>
2024-06-21 07:57:07 +0545

    Handling null exception

---
+++ Chapter-3/HandlingExceptions/Program.cs
@@ -0,0 +1,14 @@
+﻿WriteLine("Before Parsing: ");
+Write("What is your age? ");
+string? input = ReadLine();
+
+try{
+    if(input is null){
+        WriteLine("You did not enter a value so the app has ended.");
+        return;
+    }
+    int age = int.Parse(input!);
+    WriteLine($"You are {age} years old");
+}catch{
+    WriteLine("Sorry cannot convert");
+}
