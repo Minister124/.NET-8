78c12bda1b9c2f9967d4ead308f07f71f6ec7c65
Minister124 <kushaltmg321@gmail.com>
2024-06-21 08:20:18 +0545

    Format Exception

--- Chapter-3/HandlingExceptions/Program.cs
+++ Chapter-3/HandlingExceptions/Program.cs
@@ -5,7 +5,10 @@ string? input = ReadLine();
 try{
     int age = int.Parse(input!);
     WriteLine($"You are {age} years old");
-}catch(Exception ex){
+}catch (FormatException){
+    WriteLine("The age you entered is not a valid number format.");
+}
+catch(Exception ex){
     WriteLine($"{ex.GetType()} says {ex.Message}");
 }
 WriteLine("After Parsing");
