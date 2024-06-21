bbe884818681786bb7941de0cb38525a9131627a
Minister124 <kushaltmg321@gmail.com>
2024-06-21 14:55:48 +0545

    Fizzbuzz

---
+++ Chapter-3/FizzBuzz/Program.cs
@@ -0,0 +1,15 @@
+﻿for(int i = 1; i <= 100; i++){
+    int a = i % 3;
+    int b = i % 5;
+    if(a == 0 && b == 0){
+        Write("FizzBuzz,");
+    }
+    else if(a == 0){
+        Write("Buzz,");
+    }else if (b == 0){
+        Write("Fizz,");
+    }
+    else{
+        Write($"{i},");
+    }
+}
