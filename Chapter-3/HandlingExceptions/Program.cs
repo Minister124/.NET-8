7be68527b27a9612cd1f30d9c4670d0065a63f02
Minister124 <kushaltmg321@gmail.com>
2024-06-21 08:17:24 +0545

    Exception

--- Chapter-3/HandlingExceptions/Program.cs
+++ Chapter-3/HandlingExceptions/Program.cs
@@ -1,14 +1,11 @@
 ﻿WriteLine("Before Parsing: ");
-Write("What is your age? ");
+Write("What is your age? = ");
 string? input = ReadLine();
 
 try{
-    if(input is null){
-        WriteLine("You did not enter a value so the app has ended.");
-        return;
-    }
     int age = int.Parse(input!);
     WriteLine($"You are {age} years old");
-}catch{
-    WriteLine("Sorry cannot convert");
+}catch(Exception ex){
+    WriteLine($"{ex.GetType()} says {ex.Message}");
 }
+WriteLine("After Parsing");
