456c6e5bb5f2135264bf639a43435523f363c86d
Minister124 <kushaltmg321@gmail.com>
2024-06-21 15:36:31 +0545

    Input Division With exceptions

---
+++ Chapter-3/InputDivision/Program.cs
@@ -0,0 +1,31 @@
+﻿Write("Enter a Number between 0 and 255: ");
+string firstAmount = ReadLine()!;
+if (string.IsNullOrEmpty(firstAmount)) return;
+
+Write("Enter Another Number Between 0 and 255: ");
+string secondAmount = ReadLine()!;
+if (string.IsNullOrEmpty(secondAmount)) return;
+
+try
+{
+    decimal firstAmountValue = decimal.Parse(firstAmount);
+    decimal secondAmountValue = decimal.Parse(secondAmount);
+    if (firstAmountValue >= 0 && firstAmountValue <= 255 && secondAmountValue >= 0 && secondAmountValue <= 255){
+        decimal result = firstAmountValue / secondAmountValue;
+        WriteLine($"{firstAmountValue} divided by {secondAmountValue} is {result}");
+    }else{
+        WriteLine("Please Enter The Valid Value in range 0 to 255.");
+    }
+}
+catch(FormatException)
+{
+    WriteLine("FormatException: Input string was not in a correct format");
+}
+catch(OverflowException)
+{
+    WriteLine("OverflowException: Value was too large");
+}
+catch(DivideByZeroException)
+{
+    WriteLine("DivideByZeroException: This occured because the number was divided by 0");
+}
