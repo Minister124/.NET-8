e54b5791c463a9f8e7e68f44a90a38ffe4ce99ac
Minister124 <kushaltmg321@gmail.com>
2024-06-24 14:31:59 +0545

    Namespace

--- Chapter-4/TopLevelFunctions/Program.cs
+++ Chapter-4/TopLevelFunctions/Program.cs
@@ -1,9 +1,17 @@
-﻿namespace TopLevelFunctions;
+﻿using static System.Console;
+
+namespace TopLevelFunctions;
 
 class Program
 {
+
+    static void MyNameSpace(){
+        WriteLine("Namespace of program class: {0}",
+        arg0: typeof(Program).Namespace);
+    }
     static void Main(string[] args)
     {
+        MyNameSpace();
         Console.WriteLine("Hello, World!");
     }
 }
