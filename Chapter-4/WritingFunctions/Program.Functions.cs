a4f8bcb4df7cd973adeae7255be16dc9ad08a6b4
Minister124 <kushaltmg321@gmail.com>
2024-06-24 15:33:05 +0545

    Multiplication Functions

---
+++ Chapter-4/WritingFunctions/Program.Functions.cs
@@ -0,0 +1,8 @@
+﻿partial class Program{
+    static void MultiplicationTable(int num, int rows = 12){
+        for(int i = 1; i <= rows; i++){
+            WriteLine($"{num} x {i} = {num*i}");
+        }
+        WriteLine();
+    }
+}
--- Chapter-4/WritingFunctions/Program.cs
+++ Chapter-4/WritingFunctions/Program.cs
@@ -1,2 +1,2 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿MultiplicationTable(7);
+MultiplicationTable(8);
