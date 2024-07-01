7b1ffc32899c2ee936472d6aba67895ec6e23106
Minister124 <kushaltmg321@gmail.com>
2024-07-01 14:27:09 +0545

    Call Stack Exception Handling

---
+++ Chapter-4/CallStackExceptionHandlinglib/Processor.cs
@@ -0,0 +1,16 @@
+﻿using static System.Console;
+
+namespace CallStackExceptionHandlinglib;
+
+public class Processor
+{
+    public static void Gamma(){ //public can be called from outside the project
+        WriteLine("In Gamma");
+        Delta();
+    }
+
+    private static void Delta(){ //private so that it can't be called from outside the project
+        WriteLine("In Delta");
+        File.OpenText("Bad File Path");
+    }
+}
