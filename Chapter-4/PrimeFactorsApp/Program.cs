58f50a05fcaa16b0a629449d258a4db8a4c3d59b
Minister124 <kushaltmg321@gmail.com>
2024-07-02 21:32:08 +0545

    Try Parse

--- Chapter-4/PrimeFactorsApp/Program.cs
+++ Chapter-4/PrimeFactorsApp/Program.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using static System.Console;
+using PrimeFactorsLib;
+
+Write("Enter the Number Between 1 and 1000: ");
+
+if(int.TryParse(ReadLine(), out int number)){
+    WriteLine(format: "Prime Factors of {0} are: {1}",
+    arg0: number,
+    arg1: Prime.PrimeFactors(number));
+}
