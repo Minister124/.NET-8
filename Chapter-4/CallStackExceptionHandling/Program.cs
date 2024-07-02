2e90972fcfc0ef1bcbae20dd2c112dcd166123dd
Minister124 <kushaltmg321@gmail.com>
2024-07-02 08:24:29 +0545

    Exception Handling in functions

---
+++ Chapter-4/CallStackExceptionHandling/Program.cs
@@ -0,0 +1,19 @@
+﻿using CallStackExceptionHandlinglib;
+using static System.Console;
+
+WriteLine("In Main");
+Alpha();
+
+void Alpha(){
+    WriteLine("In Alpha");
+    try{
+        Beta();
+    }catch(Exception ex){
+        WriteLine($"Caught exception: {ex.Message}");
+    }
+}
+
+void Beta(){
+    WriteLine("In Beta");
+    Processor.Gamma();
+}
