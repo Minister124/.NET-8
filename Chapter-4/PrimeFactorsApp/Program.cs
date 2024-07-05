b2f57df2aa360c22ec7fdc9b224b08f16838a3f8
Minister124 <kushaltmg321@gmail.com>
2024-07-05 09:00:48 +0545

    Changes

--- Chapter-4/PrimeFactorsApp/Program.cs
+++ Chapter-4/PrimeFactorsApp/Program.cs
@@ -6,5 +6,5 @@ Write("Enter the Number Between 1 and 1000: ");
 if(int.TryParse(ReadLine(), out int number)){
     WriteLine(format: "Prime Factors of {0} are: {1}",
     arg0: number,
-    arg1: Prime.PrimeFactors(number));
+    arg1: PrimeNumber.PrimeFactors(number));
 }
