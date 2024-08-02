66f1cbd72f8790c03e4f5633ee5bd740a01b2514
Minister124 <kushaltmg321@gmail.com>
2024-08-02 13:33:54 +0545

    Dead

--- Chapter-5/PeopleApp/Program.cs
+++ Chapter-5/PeopleApp/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using PacktLibraryNetStandard2;
+
+ConfigureConsole(); // Sets Current culture to US english
+
+//Alternatives:
+// ConfigureConsole(useComputerCulture: true); // Sets Current culture to computer's culture
+//ConfigureConsole(culture: "fr-FR"); // Use French Culture.
+
+Person bob = new();
+WriteLine(bob); //Implicit call to ToString();
+// WriteLine(bob.ToString()); Does the same thing.
