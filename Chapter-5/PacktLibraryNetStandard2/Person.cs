fdb4c2c4a9e8c8c05669b382ba721e94d30d9290
Minister124 <kushaltmg321@gmail.com>
2024-08-04 13:20:36 +0545

    Fields

--- Chapter-5/PacktLibraryNetStandard2/Person.cs
+++ Chapter-5/PacktLibraryNetStandard2/Person.cs
@@ -2,5 +2,15 @@
 
 public class Person : Object
 {
-    
+    #region Fields: Data or state of this person.
+    public string? Name; // ? means it can be null
+    public DateTimeOffset Born; 
+    // We have multiple choices for the data type of the Born field. .NET 6 introduced 
+    // the DateOnly type. This would store only the date without a time value. DateTime
+    // stores the date and time of when the person was born, but it varies between local 
+    // and UTC time. The best choice is DateTimeOffset, which stores the date, time, 
+    // and hours offset from Universal Coordinated Time (UTC), which is related to the 
+    // time zone. The choice depends on how much detail you need to store.
+
+    #endregion
 }
