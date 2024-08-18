41b975dc5c35fb2b2e146f1b66222838bddc6349
Minister124 <kushaltmg321@gmail.com>
2024-08-18 08:42:16 +0545

    storing data in field without object initializer

--- Chapter-5/PeopleApp/Program.cs
+++ Chapter-5/PeopleApp/Program.cs
@@ -13,6 +13,8 @@ PacktLibraryNetStandard2.Person bob = new();
 WriteLine(bob); //Implicit call to ToString();
 // WriteLine(bob.ToString()); Does the same thing.
 
+#region Setting and outputting field values
+
 bob.Name = "Khatey";
 
 bob.Born = new DateTimeOffset(
@@ -20,8 +22,11 @@ bob.Born = new DateTimeOffset(
     offset: new TimeSpan(hours: 5, minutes: 45, seconds: 0) //Nepal Time
 );
 
+#endregion
+
 WriteLine($"{bob.Name} was born on {bob.Born}.");
 
+#region Avoiding namespace conflict using Aliases and using directives
 Nep.Person p = new();
 Sun.Person f = new();
 
@@ -31,4 +36,22 @@ WriteLine(Env.CurrentDirectory);
 WriteLine(Env.ProcessorCount);
 WriteLine(Env.CommandLine);
 
+#endregion
+
+
+#region Setting field values using object initializer syntax
+
+Person MangneyBudo = new()
+{
+    Name = "Mangney Budo",
+    Born = new(1991, 4, 28, 5, 15, 45, TimeSpan.Zero)
+};
+
+WriteLine(format: "{0} was born in {1:d}", //short date.
+ arg0: MangneyBudo.Name, arg1: MangneyBudo.Born);
+
+//using interpolated string
+WriteLine($"{MangneyBudo.Name} was born in {MangneyBudo.Born:d}");
+
+#endregion
 
